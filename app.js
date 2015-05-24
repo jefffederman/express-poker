7f0f89cb5db26c3c67c17a41ea2cd7eb427bb550
Jeff Federman <jefffederman@gmail.com>
2015-05-23 23:13:42 -0400

    Add cors; return next street

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var cors = require('cors');
+app.use(cors());
 var SimpleDeck = require('./models/simple_deck');
 var Hand = require('./models/hand');
 
--- models/hand.js
+++ models/hand.js
@@ -4,12 +4,13 @@ var Hand = bookshelf.Model.extend({
   tableName: 'hands',
   cards: function(street) {
     var streetValues = {
-      'preflop': 0,
-      'flop': 1,
-      'turn': 2,
-      'river': 3
+      'preflop': { value: 0, next: 'flop'},
+      'flop': { value: 1, next: 'turn' },
+      'turn': { value: 2, next: 'river' },
+      'river': { value: 3, next: 'preflop' }
     };
-    var streetValue = streetValues[street];
+    var streetValue = streetValues[street]['value'];
+    var nextStreet = streetValues[street]['next'];
     var board = [];
     if (streetValue > 0) {
       board = board.concat(this.get('flop'))
@@ -22,6 +23,7 @@ var Hand = bookshelf.Model.extend({
     }
     return {
       handId: this.get('id'),
+      street: nextStreet,
       player1: this.get('hole_cards_1'),
       player2: this.get('hole_cards_2'),
       board: board
